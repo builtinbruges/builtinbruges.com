154308be6ec714a97155e504fc76a306e42843ba
Bert Devriese <bert.devriese@gmail.com>
2013-12-10 14:16:35 +0100

    Added extra "progressive enhancement" fix. Logo would fade in weirdly on Chrome

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -3,8 +3,12 @@ $(document).ready(function ()
 {
 	if ($('h1.logo').length > 0)
 	{
+		$('h1.logo').css('background-image', 'none');
+
 		var logo_img = new Image();
-		logo_img.onload = function() {
+		logo_img.onload = function()
+		{
+			$('h1.logo').css('background-image', 'url('+logo_img.src+')');
 			$('h1.logo').addClass('animation')
 		}
 
